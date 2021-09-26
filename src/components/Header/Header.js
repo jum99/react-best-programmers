58b60b567e764cb6dce1b7b8a684ccdc119f2c6b
jum99 <ejumibegum@gmail.com>
2021-09-26 23:51:23 +0600

    last commit done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,6 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header-container">
-            <div className="navbar" >
-                <a className="logo" href="/home">Logo</a>
-                <div className="nav">
-                    <a href="/home">Home</a>
-                    <a href="/programmer">Programmer</a>
-                    <a href="/about">About</a>
-                </div>
-            </div>
             <div className="header-human-club">
                 <h2>Greatest Programmers Of All Time</h2>
                 <p>A good programmer is someone who always looks both ways before crossing a one-way street</p>
